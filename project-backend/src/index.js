a1907eed7912ac54bbc9e8876471631ada69a06c
ShubhGithub07 <127323514+ShubhGithub07@users.noreply.github.com>
2024-03-22 08:30:07 +0530

    M2 of database connection

--- project-backend/src/index.js
+++ project-backend/src/index.js
@@ -1,5 +1,19 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv).config({path: './env'}) // one method to connect dotenv
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+
+
+
+
+
+
 
 
 
