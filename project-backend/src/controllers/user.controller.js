6fbd6abaa245b23209443cd77a6462de28244cfa
ShubhGithub07 <127323514+ShubhGithub07@users.noreply.github.com>
2024-03-23 20:17:05 +0530

    logic building

--- project-backend/src/controllers/user.controller.js
+++ project-backend/src/controllers/user.controller.js
@@ -1,9 +1,34 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  // get user details from frontend
+  // validation - non empty
+  // check if user already exists: username, email
+  // check for images, avatar
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field from response
+  // check for user creation
+  // return response
+
+  const { fullname, username, email, password } = req.body;
+  //   console.log("email: ", email);
+
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with email or username already exists");
+  }
 });
 
 export { registerUser };
