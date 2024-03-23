3a179c01d1db554ff3aafd67e85f40dc2e81bb7c
ShubhGithub07 <127323514+ShubhGithub07@users.noreply.github.com>
2024-03-23 19:19:32 +0530

    routing, postman, thunderClient

--- project-backend/src/app.js
+++ project-backend/src/app.js
@@ -12,8 +12,14 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" })); // from, json file data config
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
