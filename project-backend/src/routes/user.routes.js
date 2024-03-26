59d6789c722864d26cca79166c84127cca8b64ae
ShubhGithub07 <127323514+ShubhGithub07@users.noreply.github.com>
2024-03-26 09:51:25 +0530

    add refreshAccessToken

--- project-backend/src/routes/user.routes.js
+++ project-backend/src/routes/user.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -26,5 +27,7 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
